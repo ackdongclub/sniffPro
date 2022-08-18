078306fa5956d98e7dea3c5c590f7fd8c2005fe5
ackdongclub <afreshbreeze21@naver.com>
2022-08-18 23:43:15 +0900

    Initial commit

---
+++ src/main/java/project01/sniffPro/SniffProApplication.java
@@ -0,0 +1,13 @@
+package project01.sniffPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SniffProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SniffProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project01/sniffPro/SniffProApplicationTests.java
@@ -0,0 +1,13 @@
+package project01.sniffPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SniffProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
